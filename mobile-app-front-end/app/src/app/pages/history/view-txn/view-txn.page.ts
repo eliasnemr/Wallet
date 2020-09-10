3b5ba0486e6b545f72dfc983d9a3c2fdd86f2267
Elias Nemr <eliasnemr@gmail.com>
2020-09-10 15:56:19 +0300

    added userData storage, revamped history, and txn-details, more layout touchs

--- mobile-app-front-end/app/src/app/pages/history/view-txn/view-txn.page.ts
+++ mobile-app-front-end/app/src/app/pages/history/view-txn/view-txn.page.ts
@@ -1,6 +1,8 @@
-import { NavController, NavParams } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
+import { NavController, NavParams, ToastController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 
+declare var Minima: any;
 @Component({
   selector: 'app-view-txn',
   templateUrl: './view-txn.page.html',
@@ -8,13 +10,82 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewTXNPage implements OnInit {
 
-  txn_id: string;
+  hide: boolean = false;
+  public txn: string;
+  public id: string;
+  public relaytime: string;
+  public size: string;
+  public isinblock: boolean;
+  public blocknumber: number;
+  public isblock: boolean;
+  public nonce: string;
+  public inputs: [];
+  public outputs: [];
+  public type: string;
+  public tokenname: string;
+  public tokentotal: string;
 
-  constructor() {
-    // this.txn_id = this.navParams.get("txn");
-   }
+
+
+  constructor(public route: ActivatedRoute, public toastController: ToastController) { 
+
+    this.txn = this.route.snapshot.paramMap.get("id");
+
+  }
 
   ngOnInit() {
+    Minima.cmd('txpowinfo '+ this.txn, (res: any) =>{
+      this.id = res.response.txpow.txpowid;
+      this.relaytime = new Date(res.response.txpow.header.timesecs*1000).toISOString();
+      this.size = res.response.txpow.size;
+      this.isblock = res.response.txpow.isblock;
+      this.isinblock = res.response.isinblock;
+      this.blocknumber = res.response.inblock;
+      this.nonce = res.response.txpow.header.nonce;
+      this.inputs = res.response.txpow.body.txn.inputs;
+      this.outputs = res.response.txpow.body.txn.outputs;
+      // is token created
+      if(res.response.txpow.body.txn.tokengen){
+        this.type = "Token Creation.";
+        this.tokenname = res.response.txpow.body.txn.tokengen.token;
+        this.tokentotal = res.response.txpow.body.txn.tokengen.total;
+      } else {
+        this.type = "Value Transfer.";
+      }
+    })
   }
+  
+  shout() {
+    if(this.hide == true){
+      this.hide = false;
+    } else {
+      this.hide = true;
+    }
+  }
+
+  copyToClipPWA(text: string) {
+    document.addEventListener('copy', (e: ClipboardEvent) => {
+      e.clipboardData.setData('text/plain', text);
+      this.presentToast("Copied to Clipboard", "success");
+      e.preventDefault();
+      document.removeEventListener('copy', null);
+    });
+    document.execCommand('copy');
+  }
+
+  async presentToast(msg: string, type: string) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 4000,
+      color: type,
+      keyboardClose: true,
+      translucent: true,
+      position:  'bottom'
+    });
+    toast.present();
+  }
+
+
+  
 
 }
