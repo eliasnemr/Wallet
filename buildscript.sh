a5b73138e849a4c609a3f14e32bae417b7cec70a
Elias Nemr <eliasnemr@gmail.com>
2020-10-05 15:08:43 +0300

    restructuring

--- buildscript.sh
+++ buildscript.sh
@@ -4,6 +4,13 @@ rm -rf ./minidapp
 
 mkdir minidapp
 
-zip -r ./minidapp/wallet.minidapp mobile-app-front-end/app/www
+mkdir minidapp/wallet
+
+cp -rf mobile-app-front-end/app/www/* minidapp/wallet
+
+cp overlay.jpeg minidapp/wallet
+cp minidapp.conf minidapp/wallet
+
+zip -r ./minidapp/wallet.minidapp minidapp/wallet
 
 
