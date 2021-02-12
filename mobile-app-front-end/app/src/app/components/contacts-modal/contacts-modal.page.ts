866a7023526b35c53461a7981e3d2f2e10b29861
eliasnemr <eliasnemr@gmail.com>
2021-02-12 17:20:12 +0200

    fixed icon crashing wallet issue 84

--- mobile-app-front-end/app/src/app/components/contacts-modal/contacts-modal.page.ts
+++ mobile-app-front-end/app/src/app/components/contacts-modal/contacts-modal.page.ts
@@ -43,7 +43,7 @@ export class ContactsModalPage implements OnInit {
       DESCRIPTION: ['', [Validators.maxLength(255)]],
       AVATAR: ['', [
             Validators.maxLength(255),
-            Validators.pattern('^https?://(?:[a-z0-9\-]+\.)+[a-z]{2,6}(?:/[^/#?]+)+\.(?:jpg|gif|png)$'),
+            Validators.pattern('(http(s?):)([\\/|\\.|\\w|\\s|\\-])*\.(?:jpg|png)$'),
            ]
       ]
     });
