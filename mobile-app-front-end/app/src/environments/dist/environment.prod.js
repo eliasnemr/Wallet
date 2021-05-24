a469c7b81255da49a14816fffd5d57c09426904a
Elias <eliasnemr@gmail.com>
2021-05-24 17:39:46 +0300

    darkMode is now media-query, much layout fixs, inputs enlargened, error message+icon enhanced, view-txn-page routing enhanced + more

--- mobile-app-front-end/app/src/environments/dist/environment.prod.js
+++ mobile-app-front-end/app/src/environments/dist/environment.prod.js
@@ -2,7 +2,7 @@
 exports.__esModule = true;
 exports.environment = void 0;
 exports.environment = {
-    wallet_version: 'v1.0.80',
+    wallet_version: 'v1.1.0',
     production: true,
     newLine: '/(\r\n|\n|\r)/gm'
 };
