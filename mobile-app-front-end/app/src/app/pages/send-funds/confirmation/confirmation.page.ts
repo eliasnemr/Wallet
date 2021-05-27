dfad77ef2fa5ebcb13d11481eb762ff183a5bc36
Elias <eliasnemr@gmail.com>
2021-05-27 11:27:05 +0300

    Confirmation page for sendPage finished.  UI/UX with sending, buttons etc.  SendPage removed posting logic.

--- mobile-app-front-end/app/src/app/pages/send-funds/confirmation/confirmation.page.ts
+++ mobile-app-front-end/app/src/app/pages/send-funds/confirmation/confirmation.page.ts
@@ -1,8 +1,10 @@
+import { ToolsService } from './../../../service/tools.service';
+import { Router } from '@angular/router';
 import { SendFormObj } from './../send-funds.page';
 import { Subscription } from 'rxjs';
 import { MinimaApiService } from './../../../service/minima-api.service';
-import { MenuController } from '@ionic/angular';
-import { Component, OnInit } from '@angular/core';
+import { MenuController, IonButton } from '@ionic/angular';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Token } from 'minima';
 import * as SparkMD5 from 'spark-md5';
 
@@ -18,11 +20,16 @@ export class ConfirmationPage implements OnInit {
   public tokenIcon: string;
   public availableBalance: string;
   public avatar: string;
+  public status: string;
+  @ViewChild('confirmBtn', {static: false}) confirmBtn: IonButton;
   constructor(
     public menu: MenuController,
-    private minimaApiService: MinimaApiService) {
+    private minimaApiService: MinimaApiService,
+    private router: Router,
+    private myTools: ToolsService) {
     this.tokenIcon = '';
     this.tokenName = '';
+    this.status = 'Confirm';
   }
 
   ngOnInit() {
@@ -33,11 +40,12 @@ export class ConfirmationPage implements OnInit {
     this.$subscription =
     this.minimaApiService.$urlData.subscribe((data: SendFormObj) => {
       this.data = data;
+      // console.log(this.data);
       this.$subscription =
       this.minimaApiService.$balance.subscribe((balance: Token[]) => {
         balance.forEach((token: Token) => {
           if (token.tokenid === this.data.tokenid) {
-            console.log(token);
+            // console.log(token);
             this.tokenName = token.token;
             if (token.tokenid !== '0x00' && token.icon.length > 0) {
               this.tokenIcon = token.icon;
@@ -54,5 +62,55 @@ export class ConfirmationPage implements OnInit {
 
   createIcon(tokenid: string) {
     return this.avatar = 'https://www.gravatar.com/avatar/' + SparkMD5.hash(tokenid) + '?d=identicon';
+  }
+
+  sendFunds(data) {
+    this.status = '';
+    const dt: SendFormObj = data;
+    // console.log(data);
+    try {
+      this.post(dt);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  /**  */
+  async post(data: any) {
+    this.confirmBtn.disabled = true;
+    if (data.message !== null && ( data.message || data.message.length > 0) ) {
+      const res: any =
+      await this.minimaApiService.sendMessageTransaction(data);
+      // console.log(res);
+      if (res.status) {
+        this.myTools.presentAlert(
+            'Transaction Status',
+            'Transaction has been posted to the network!',
+            'Successful');
+        this.router.navigate(['/send-funds']);
+        this.status = 'Posted!';
+      } else {
+        setTimeout(() => {
+          this.confirmBtn.disabled = false;
+          this.status = 'Confirm';
+        }, 500);
+        this.myTools.presentAlert('Transaction Status', res.message, 'Failed');
+      }
+    } else {
+      const res: any = await this.minimaApiService.sendFunds(data);
+      if (res.status) {
+        this.myTools.presentAlert(
+            'Transaction Status',
+            'Transaction has been posted to the network!',
+            'Successful');
+        this.router.navigate(['/send-funds']);
+        this.status = 'Posted!';
+      } else {
+        setTimeout(() => {
+          this.confirmBtn.disabled = false;
+          this.status = 'Confirm';
+        }, 500);
+        this.myTools.presentAlert('Transaction Status', res.message, 'Failed');
+      }
+    }
   }
 }
