b2441f2b97b23fa4fb39662e75e010c9e85ca0a9
Elias <eliasnemr@gmail.com>
2021-06-01 11:25:23 +0300

    Latest Minidapp built v1.1.2

--- mobile-app-front-end/app/src/environments/dist/environment.prod.js
+++ mobile-app-front-end/app/src/environments/dist/environment.prod.js
@@ -2,7 +2,7 @@
 exports.__esModule = true;
 exports.environment = void 0;
 exports.environment = {
-    wallet_version: 'v1.1.1',
+    wallet_version: 'v1.1.2',
     production: true,
     newLine: '/(\r\n|\n|\r)/gm'
 };
--- mobile-app-front-end/app/src/environments/environment.prod.ts
+++ mobile-app-front-end/app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  wallet_version: 'v1.1.1',
+  wallet_version: 'v1.1.2',
   production: true,
   newLine: '/(\r\n|\n|\r)/gm',
-}
+};
